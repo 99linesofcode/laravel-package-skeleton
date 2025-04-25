10f670d4bb8fe6d0a4cf1857a7462bafdebd5a60
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-04-25 23:38:29 +0400

    feat(post): example Models/Post and Factories/PostFactory
    As an example of how to override the model factory directory.

---
+++ src/Domain/Models/Post.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Linesofcode\LaravelPackageSkeleton\Domain\Models;
+
+use Auth\Domain\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @mixin IdeHelperPost
+ */
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'author_id',
+        'title',
+        'body',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+        'author_id' => 'int',
+    ];
+
+    /**
+     * Create a new factory instance for the model.
+     */
+    protected static function newFactory()
+    {
+        return FlightFactory::new();
+    }
+
+    /**
+     * Get the author of the post.
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+}
---
+++ src/Infrastructure/Factories/PostFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Linesofcode\LaravelPackageSkeleton\Infrastructure\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Blog\Domain\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'author_id' => 1,
+            'title' => fake()->sentence(),
+            'body' => fake()->sentences(),
+        ];
+    }
+
+    /**
+     * Indicate that the post was published.
+     */
+    public function published(): Factory
+    {
+        return $this->state(function (): array {
+            return [
+                'published_at' => now(),
+            ];
+        });
+    }
+}
