20d737b75c86d5f0d155c3dd013928a4966e27b5
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-01-22 13:12:23 +0100

    chore(filament): replace livewire, flux and volt with batteries included filament

---
+++ src/BlogPanelProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blog;
+
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\AuthenticateSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages\Dashboard;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Widgets\AccountWidget;
+use Filament\Widgets\FilamentInfoWidget;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+class BlogPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->default()
+            ->id('admin')
+            ->path('admin')
+            ->login()
+            ->discoverResources(in: __DIR__.'/App/Filament/Resources', for: 'Blog\Filament\Resources')
+            ->discoverPages(in: __DIR__.'/App/Filament/Pages', for: 'Blog\Filament\Pages')
+            ->pages([
+                Dashboard::class,
+            ])
+            ->discoverwidgets(in: __DIR__.'/app/filament/widgets', for: 'blog\filament\widgets')
+            ->widgets([
+                accountwidget::class,
+                filamentinfowidget::class,
+            ])
+            ->middleware([
+                encryptcookies::class,
+                addqueuedcookiestoresponse::class,
+                startsession::class,
+                authenticatesession::class,
+                shareerrorsfromsession::class,
+                verifycsrftoken::class,
+                substitutebindings::class,
+                disablebladeiconcomponents::class,
+                dispatchservingfilamentevent::class,
+            ])
+            ->authmiddleware([
+                authenticate::class,
+            ]);
+    }
+}
