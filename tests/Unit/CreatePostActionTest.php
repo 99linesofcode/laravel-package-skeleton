dee9efcf9cdc54c14276c27ae5a2010bb8720f60
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-02-26 00:11:40 +0100

    feat(pest): unit/feature/browser testing using Pest 4 and Playwright

--- tests/Unit/CreatePostActionTest.php
+++ tests/Unit/CreatePostActionTest.php
@@ -2,25 +2,18 @@
 
 declare(strict_types=1);
 
-namespace Lines\Skeleton\Tests\Unit;
+uses(\Lines\Skeleton\Tests\TestCase::class);
 
 use Lines\Skeleton\Domain\Actions\CreatePostAction;
 use Lines\Skeleton\Domain\DataTransferObjects\CreatePostData;
 use Lines\Skeleton\Domain\Models\Post;
-use Lines\Skeleton\Tests\TestCase;
-use PHPUnit\Framework\Attributes\Test;
 
-final class CreatePostActionTest extends TestCase
-{
-    #[Test]
-    public function __invoke(): void
-    {
-        (new CreatePostAction)(new CreatePostData(
-            author_id: 1,
-            title: 'Domain Driven and Modular Laravel Packages',
-            body: 'lorem ipsum dolor sit amat',
-        ));
+test('invoke', function () {
+    (new CreatePostAction)(new CreatePostData(
+        author_id: 1,
+        title: 'Domain Driven and Modular Laravel Packages',
+        body: 'lorem ipsum dolor sit amat',
+    ));
 
-        $this->assertModelExists(Post::class);
-    }
-}
+    self::assertModelExists(Post::class);
+});
