327b9fe8cedcc7a26c05224afc295781f0c09f54
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-02-27 17:17:16 +0100

    fix: seeding database with example user and posts

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Lines\Skeleton\Domain\Models\Post;
+use Workbench\Database\Factories\UserFactory;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        UserFactory::new()->create([
+            'name' => 'Jordy Schreuders',
+            'email' => 'jordy@schreuders.it',
+            'password' => Hash::make('pass1234'),
+        ]);
+
+        Post::factory()->count(20)->create();
+    }
+}
