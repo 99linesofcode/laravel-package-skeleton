a06b730a943d599b0264b2d3f94e9f7bfcd16768
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-03-16 02:31:36 +0100

    chore: use local User model for the time being
    This should obviously be pulled in from and added to the application
    from another package. Possibly as a dev dependency.

--- src/Domain/Models/Post.php
+++ src/Domain/Models/Post.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Lines\Auth\Domain\Models\User;
 use Lines\Skeleton\Domain\Enums\PostStatus;
 
 /**
